4749d5b5b2cf37a4f99e72693d5994dff8a680ba
bagaskara04 <sagabdespair01@gmail.com>
2025-03-07 06:49:46 +0700

    eloquent ORM

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,9 +10,17 @@ class UserController extends Controller
 {
     public function index(){
         // $data = [
-        //     'nama' => 'Pelanggan Pertama',
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
         // ];
-        // UserModel::where('username', 'customer-1')->update($data);
+        // UserModel::insert($data);
+
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data);
 
         $user = UserModel::all();
         return view('user',['data' => $user] );
