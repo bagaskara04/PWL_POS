0f69a9ab3844978202afd8798119b9b323e75be1
bagaskara04 <sagabdespair01@gmail.com>
2025-03-07 20:07:03 +0700

    Praktikum 1 fillable

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,4 +11,6 @@ class UserModel extends Model
 
     protected $table = 'm_user'; //mendefinisikan nama tabel
     protected $primaryKey = 'user_id'; //mendefinisikan primary key
+
+    protected $fillable = ['level_id', 'username', 'nama'];
 }
