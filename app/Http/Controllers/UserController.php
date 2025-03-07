92287d0c7cf1642e578b38e2a5eb095381b9c030
bagaskara04 <sagabdespair01@gmail.com>
2025-03-07 20:32:13 +0700

    Praktikum 2.1 Retrieving Single Modles

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,15 +17,22 @@ class UserController extends Controller
         // ];
         // UserModel::insert($data);
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all();
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user',['data' => $user] );
+
+        $user = UserModel::findOr(21, ['username', 'nama'], function(){
+            abort(404);
+        });
+
         return view('user',['data' => $user] );
+
     }
 }
