6f9a403554b9db047cc6307d4368b26bcfcd8ee6
bagaskara04 <sagabdespair01@gmail.com>
2025-03-08 12:22:01 +0700

    Praktikum 2.7-Relationships

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -56,7 +56,7 @@ class UserController extends Controller
         // $user->wasChanged('nama'); // false
         // dd($user->wasChanged(['nama', 'username'])); // true
 
-        $user = UserModel::all();
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
     }
 
